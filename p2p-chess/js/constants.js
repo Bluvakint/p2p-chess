21abdbd6c1ef829c287a1f43c625eda336ecc3d5
Bluvakint <bluk52556@gmail.com>
2026-09-06 03:14:13 +0500

    Add files via upload

---
+++ p2p-chess/js/constants.js
@@ -0,0 +1,9 @@
+export const PIECES = {
+    'w': { 'k': '♔', 'q': '♕', 'r': '♖', 'b': '♗', 'n': '♘', 'p': '♙' },
+    'b': { 'k': '♚', 'q': '♛', 'r': '♜', 'b': '♝', 'n': '♞', 'p': '♟' }
+};
+
+export const DEFAULT_SETTINGS = {
+    hostColor: 'white',
+    timeControl: 0
+};
