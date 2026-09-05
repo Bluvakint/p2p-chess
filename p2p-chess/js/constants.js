41bcf6a6ceffa96d76f567ae60d488980a28db00
Bluvakint <bluk52556@gmail.com>
2026-09-06 03:25:50 +0500

    Delete p2p-chess directory

--- p2p-chess/js/constants.js
+++
@@ -1,9 +0,0 @@
-export const PIECES = {
-    'w': { 'k': '♔', 'q': '♕', 'r': '♖', 'b': '♗', 'n': '♘', 'p': '♙' },
-    'b': { 'k': '♚', 'q': '♛', 'r': '♜', 'b': '♝', 'n': '♞', 'p': '♟' }
-};
-
-export const DEFAULT_SETTINGS = {
-    hostColor: 'white',
-    timeControl: 0
-};
